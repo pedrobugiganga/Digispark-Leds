9d615646ed8e3d8d8327590870ce087a1fb0b6eb
Pedro Pereira <43342401+pedroldf@users.noreply.github.com>
2018-10-17 13:36:17 -0300

    Add files via upload

---
+++ attiny85_leds.ino
@@ -0,0 +1,34 @@
+#define acelerador 0
+
+int i;
+int j;
+int button;
+
+
+void setup() {
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(acelerador, INPUT);
+
+}
+void loop() {
+  for(i=0;i<5;i++){
+  digitalWrite(i,HIGH);
+  button = digitalRead(acelerador);
+  if(button == LOW){
+  delay (50);}
+  else {delay(100);}
+  digitalWrite(i,LOW);}
+
+for(j=4;j>1;j--){
+  digitalWrite(j,HIGH);
+  button = digitalRead(acelerador);
+  if(button == LOW){
+  delay (50);}
+  else {delay(100);}
+  digitalWrite(j,LOW);}
+
+}
+  
